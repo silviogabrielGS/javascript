3a7c355d32f85bf1343e82715639557ea781f4b9
silviogabrielGS <silviogabrielgsantana@gmail.com>
2022-08-25 15:33:17 -0300

    atualizando exercício JS

--- ex015/script.js
+++ ex015/script.js
@@ -6,9 +6,12 @@ function carregar() {
     msg.innerHTML = "agora são " + hora + " horas"
     if (hora > 0 && hora < 12) {
         img.src = "imagens/foto-manhã.jpg"
+        document.body.style.background = "#ca9e23"
     } else if (hora >= 12 && hora < 18) {
         img.src = "imagens/foto-tarde.jpg"
+        document.body.style.background = "#b46316"
     } else if (hora >= 18 && hora < 25) {
         img.src = "imagens/foto-noite.jpg"
+        document.body.style.background = "rgb(52, 53, 66)"
     }
 }
