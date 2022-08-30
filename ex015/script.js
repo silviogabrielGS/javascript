45f83229b5337a606098fff7e3be337515387886
silviogabrielGS <silviogabrielgsantana@gmail.com>
2022-08-30 15:33:38 -0300

    atualizando ex05

--- ex015/script.js
+++ ex015/script.js
@@ -2,7 +2,7 @@ function carregar() {
     var msg = document.getElementById("msg")
     var img = document.getElementById("imagem")
     var agora = new Date()
-    var hora = agora.getHours()
+    var hora = agora.getHours() 
     msg.innerHTML = "agora são " + hora + " horas"
     if (hora > 0 && hora < 12) {
         img.src = "imagens/foto-manhã.jpg"
