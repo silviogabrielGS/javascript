add4ae016e01bb7981b7cf05f42ca914254bd6c2
Silvio Gabriel <silviogabrielgsantana@gmail.com>
2022-11-29 15:09:25 -0300

    criando exercicio 20

--- ex020/script.js
+++ ex020/script.js
@@ -1,4 +1,4 @@
-function contar() {
+function montar() {
     var ini = document.getElementById("inicio")
     var fim = document.getElementById("fim")
     var passo = document.getElementById("passo")
@@ -7,17 +7,8 @@ function contar() {
     if (ini.value.length == 0) {
         window.alert("preencha corretamente os dados")
     } else {
-         res.innerHTML = ("contando...")
-         var i = Number(ini.value)
-         var f = Number (fim.value)
-         var p = Number (passo.value)
+       
          
-
-         for (var c = i ; i <= f ; c += p) {
-             i = i + p
-            res.innerHTML +=  c
-            
-         }
         
     }
     }
